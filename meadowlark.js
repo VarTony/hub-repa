e3ccfcf70cf5ba6188cebe2a8c8e165df9d14b55
Steemit <littlesalo3@gmail.com>
2017-03-29 17:41:17 +0300

    Go to hub-repa

---
+++ meadowlark.js
@@ -0,0 +1,58 @@
+var fortune = require("./lib/fortune.js");
+
+
+var express  = require("express");
+
+var app = express();
+
+//Установка механизма представления handlebars
+
+
+var handlebars = require("express-handlebars")
+  .create({ defaultLayout: "main"});
+app.engine("handlebars",  handlebars.engine);
+app.set("view engine", "handlebars");
+
+
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(express.static(__dirname + "./public"));
+
+app.use(function(req, res, next){
+  res.locals.showTests = app.get("env") !== "prodaction" &&
+    req.query.test === "1";
+   next();
+});
+
+
+
+app.get("/",  function(req, res){
+  res.render("home");
+});
+
+app.get("/about",  function(req, res){
+res.render("about", {fortunes: fortune.getFortune()});
+});
+
+//пользовательская страница 404
+
+app.use(function(req, res, next){
+  res.status(404);
+  res.render("404");
+});
+
+//Пользовательская страница 500
+
+app.use(function(err,  req, res, next){
+  console.error(err,stack);
+  res.status(500);
+  res.render("500");
+});
+
+app.listen(app.get("port"),  function(){
+
+console.log("Express запущен на http://localhost:" +
+app.get("port") + "; Нажмите ^C для завершения." )
+
+});
